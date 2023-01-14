e52928fa5527f4e19c1fcbab6632a617358268eb
Aleksei Lobachev <90100576+LexLobachev@users.noreply.github.com>
2023-01-14 18:50:38 +0400

    wrap code into ifmain

--- main.py
+++ main.py
@@ -5,32 +5,38 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-template = env.get_template('template.html')
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-age_token = lambda age_to: (
-        (age_to in range(5, 20)) and 'лет' or
-        (age_to == 111) and 'лет' or
-        (1 in (age_to, (last_dig := age_to % 10))) and 'год' or
-        ({age_to, last_dig} & {2, 3, 4}) and 'года' or 'лет')
-dif_years = datetime.date.today().year - 1920
+    template = env.get_template('template.html')
 
-excel_data_df = pandas.read_excel('wine3.xlsx', sheet_name='Лист1')
-all_liquids = excel_data_df.fillna('').groupby(['Категория'], sort=False)\
-    .apply(lambda x: x.to_dict(orient='records')).to_dict()
+    age_token = lambda age_to: (
+            (age_to in range(5, 20)) and 'лет' or
+            (age_to == 111) and 'лет' or
+            (1 in (age_to, (last_dig := age_to % 10))) and 'год' or
+            ({age_to, last_dig} & {2, 3, 4}) and 'года' or 'лет')
+    dif_years = datetime.date.today().year - 1920
 
-rendered_page = template.render(
-    count_of_years=dif_years,
-    correct_russian_year=age_token(dif_years),
-    all_liquids=all_liquids,
-)
+    excel_data_df = pandas.read_excel('wine3.xlsx', sheet_name='Лист1')
+    all_liquids = excel_data_df.fillna('').groupby(['Категория'], sort=False)\
+        .apply(lambda x: x.to_dict(orient='records')).to_dict()
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    rendered_page = template.render(
+        count_of_years=dif_years,
+        correct_russian_year=age_token(dif_years),
+        all_liquids=all_liquids,
+    )
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
